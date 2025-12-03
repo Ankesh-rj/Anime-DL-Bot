2cd3189906f01bb39a8890b88a4ff1adc00c9891
Ankesh-rj <ankeshrajkr0@gmail.com>
2025-12-03 12:28:07 +0530

    Update API credentials in config.py
    Updated API credentials for the Anime DL Telegram bot.

--- config.py
+++ config.py
@@ -5,6 +5,7 @@
 
 """ANIME DL TELEGRAM BOT CREDENTIALS"""
 
-api_id = ""
-api_hash = ""
-bot_token = ""
+api_id = "21221822"
+api_hash = "0c566392a64ef4d8f49681c4798e8391"
+bot_token = "8267076335:AAFfEIKAob0rWDtcLgajqpxlC3PfNFujBHA"
+
